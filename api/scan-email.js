1864c2485abdf88d4fd513d6ad1a1a12c715a511
Antonio050503 <antoniovelasquezcr@gmail.com>
2026-04-23 22:35:52 -0700

    Update scan-email.js

--- api/scan-email.js
+++ api/scan-email.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
 
     const listResponse = await gmail.users.messages.list({
       userId: 'me',
-      q: 'subject:RoofNotes -label:RoofNotes-Processed',
+      q: 'in:inbox subject:RoofNotes -label:RoofNotes-Processed to:antoniovelasquezcr@gmail.com',
       maxResults: 10,
     });
 
